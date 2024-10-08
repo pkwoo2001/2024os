d5dc685ab3e6247d53ae82ae970987e6d6ad8ef7
pkwoo2001 <pkwoo2001@postech.ac.kr>
2024-10-08 22:34:36 +0900

    for_test

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -29,7 +29,7 @@ void lock_acquire (struct lock *);
 bool lock_try_acquire (struct lock *);
 void lock_release (struct lock *);
 bool lock_held_by_current_thread (const struct lock *);
-
+/*checking git upload*/
 /* Condition variable. */
 struct condition 
   {
